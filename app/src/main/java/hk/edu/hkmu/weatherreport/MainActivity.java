688b56284d43925a7ef73678d4a6a3deeff37963
MrLaw001 <hkmu1123001@gmail.com>
2023-11-29 01:23:56 +0800

    set the actionbar

--- app/src/main/java/hk/edu/hkmu/weatherreport/MainActivity.java
+++ app/src/main/java/hk/edu/hkmu/weatherreport/MainActivity.java
@@ -4,12 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.MenuItem;
+
+//app bar import
+import androidx.appcompat.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the title for the ActionBar
+        getSupportActionBar().setTitle("My Title");
+
+        // Enable the Up button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Toolbar(cannot use)
+        /*
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Navigate back to previous screen
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
