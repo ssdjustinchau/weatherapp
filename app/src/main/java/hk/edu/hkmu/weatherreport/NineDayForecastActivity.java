1fc7b9d6ddf7d1c8a8fa8074e0429c221d3a15b5
s1285013 <s12850138@gmail.com>
2023-11-30 15:05:03 +0800

    add meun bar to NineDayForecastActitiy, remove local.properties from github

--- app/src/main/java/hk/edu/hkmu/weatherreport/NineDayForecastActivity.java
+++ app/src/main/java/hk/edu/hkmu/weatherreport/NineDayForecastActivity.java
@@ -1,10 +1,20 @@
 package hk.edu.hkmu.weatherreport;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
 import java.util.ArrayList;
 
 public class NineDayForecastActivity extends AppCompatActivity {
@@ -37,4 +47,61 @@ public class NineDayForecastActivity extends AppCompatActivity {
             }
         }).start();
     }
+
+    private void handleVibrationPreference() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean shouldVibrate = sharedPreferences.getBoolean("pref_key_vibration", true);
+        if (shouldVibrate) {
+            // Vibrate the device. You'll need the VIBRATE permission for this.
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= 26) {
+                vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+                Log.d("Vibration", "Vibration started");
+            } else {
+                vibrator.vibrate(200);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //case android.R.id.home:å
+            // Navigate back to previous screen
+            //onBackPressed();
+            //return true;
+            case R.id.action_home:
+                // Create an Intent to start MainActivity
+                handleVibrationPreference();
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
+                return true;
+            case R.id.action_about:
+                // Create an Intent to start AboutActivity
+                handleVibrationPreference();
+                Intent aboutIntent = new Intent(this, AboutActivity.class);
+                startActivity(aboutIntent);
+                return true;
+            case R.id.action_settings:
+                // Create an Intent to start SettingsActivity
+                handleVibrationPreference();
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            case R.id.action_exit:
+                // Exit the app
+                finishAffinity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
